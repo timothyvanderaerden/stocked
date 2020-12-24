8f8017288fd5a356e71c54afec769a6fb2bae5c5
Timothy Vanderaerden <timothyvanderaerden@gmail.com>
2020-12-24 19:33:23 +0100

    [LiveView] Basic Product implementation
    Standard generated Product implementation.

---
+++ lib/stocked_web/live/product_live/index.ex
@@ -0,0 +1,46 @@
+defmodule StockedWeb.ProductLive.Index do
+  use StockedWeb, :live_view
+
+  alias Stocked.Catalog
+  alias Stocked.Catalog.Product
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :product_collection, list_product())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(:page_title, "Edit Product")
+    |> assign(:product, Catalog.get_product!(id))
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Product")
+    |> assign(:product, %Product{})
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Product")
+    |> assign(:product, nil)
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    product = Catalog.get_product!(id)
+    {:ok, _} = Catalog.delete_product(product)
+
+    {:noreply, assign(socket, :product_collection, list_product())}
+  end
+
+  defp list_product do
+    Catalog.list_product()
+  end
+end
